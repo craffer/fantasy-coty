2951072296713683afdc17b74fc108b2c139046d
Conor Rafferty <craffer@umich.edu>
2019-12-22 14:43:49 -0500

    Replace magic numbers with variables

--- tests/test_optimal.py
+++ tests/test_optimal.py
@@ -63,17 +63,17 @@ class TestAddToOptimal(unittest.TestCase):
 
         rb1 = copy.deepcopy(self.default_player)
         rb1.position = "RB"
-        rb1.points = 12
+        rb1.points = 20
         self.optimal = add_to_optimal(self.optimal, self.settings, rb1, self.flex)
-        self.assertEqual(self.optimal["RB"][0].points, 12)
+        self.assertEqual(self.optimal["RB"][0].points, rb1.points)
         self.assertLess(len(self.optimal["RB"]), self.settings["RB"])
 
         rb2 = copy.deepcopy(self.default_player)
         rb2.position = "RB"
-        rb2.points = 10
+        rb2.points = 12
         self.optimal = add_to_optimal(self.optimal, self.settings, rb2, self.flex)
-        self.assertIn(12, [x.points for x in self.optimal["RB"]])
-        self.assertIn(10, [x.points for x in self.optimal["RB"]])
+        self.assertIn(rb1.points, [x.points for x in self.optimal["RB"]])
+        self.assertIn(rb2.points, [x.points for x in self.optimal["RB"]])
         self.assertEqual(len(self.optimal["RB"]), self.settings["RB"])
 
         # test adding to FLEX when less than the other 2 RBs
@@ -81,21 +81,32 @@ class TestAddToOptimal(unittest.TestCase):
         rb3.position = "RB"
         rb3.points = 8
         self.optimal = add_to_optimal(self.optimal, self.settings, rb3, self.flex)
-        self.assertIn(12, [x.points for x in self.optimal["RB"]])
-        self.assertIn(10, [x.points for x in self.optimal["RB"]])
+        self.assertIn(rb1.points, [x.points for x in self.optimal["RB"]])
+        self.assertIn(rb2.points, [x.points for x in self.optimal["RB"]])
         self.assertEqual(len(self.optimal["RB"]), self.settings["RB"])
-        self.assertIn(8, [x.points for x in self.optimal[self.flex]])
+        self.assertIn(rb3.points, [x.points for x in self.optimal[self.flex]])
         self.assertEqual(len(self.optimal[self.flex]), self.settings[self.flex])
 
         # test bumping one RB from RB2 to FLEX
         rb4 = copy.deepcopy(self.default_player)
         rb4.position = "RB"
-        rb4.points = 11
+        rb4.points = 16
         self.optimal = add_to_optimal(self.optimal, self.settings, rb4, self.flex)
-        self.assertIn(12, [x.points for x in self.optimal["RB"]])
-        self.assertIn(11, [x.points for x in self.optimal["RB"]])
+        self.assertIn(rb1.points, [x.points for x in self.optimal["RB"]])
+        self.assertIn(rb4.points, [x.points for x in self.optimal["RB"]])
         self.assertEqual(len(self.optimal["RB"]), self.settings["RB"])
-        self.assertIn(10, [x.points for x in self.optimal[self.flex]])
+        self.assertIn(rb2.points, [x.points for x in self.optimal[self.flex]])
+        self.assertEqual(len(self.optimal[self.flex]), self.settings[self.flex])
+
+        # test putting something straight away in flex
+        rb5 = copy.deepcopy(self.default_player)
+        rb5.position = "RB"
+        rb5.points = 14
+        self.optimal = add_to_optimal(self.optimal, self.settings, rb5, self.flex)
+        self.assertIn(rb1.points, [x.points for x in self.optimal["RB"]])
+        self.assertIn(rb4.points, [x.points for x in self.optimal["RB"]])
+        self.assertEqual(len(self.optimal["RB"]), self.settings["RB"])
+        self.assertIn(rb5.points, [x.points for x in self.optimal[self.flex]])
         self.assertEqual(len(self.optimal[self.flex]), self.settings[self.flex])
 
 
