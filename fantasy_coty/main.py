50dcc50d405ad7250ecc70e249895d7a42adb346
Conor Rafferty <craffer@umich.edu>
2019-12-22 00:18:37 -0500

    Reformat with black

--- fantasy_coty/main.py
+++ fantasy_coty/main.py
@@ -10,16 +10,32 @@ import ff_espn_api  # pylint: disable=import-error
 
 def init_league() -> ff_espn_api.League:
     """Initialize a League object from the command line arguments."""
-    parser = argparse.ArgumentParser(description='Determine the fantasy Coach and GM of the Year.')
-    parser.add_argument('league_id', type=int, help='ESPN FF league ID, from the URL')
-    parser.add_argument('year', type=int, help='year to analyze')
-    parser.add_argument('--username', '-u', type=str, help='ESPN username, if league is private',
-                        default=None)
-    parser.add_argument('--password', '-p', type=str, help='ESPN password, if league is private',
-                        default=None)
+    parser = argparse.ArgumentParser(
+        description="Determine the fantasy Coach and GM of the Year."
+    )
+    parser.add_argument("league_id", type=int, help="ESPN FF league ID, from the URL")
+    parser.add_argument("year", type=int, help="year to analyze")
+    parser.add_argument(
+        "--username",
+        "-u",
+        type=str,
+        help="ESPN username, if league is private",
+        default=None,
+    )
+    parser.add_argument(
+        "--password",
+        "-p",
+        type=str,
+        help="ESPN password, if league is private",
+        default=None,
+    )
     args = parser.parse_args()
-    return ff_espn_api.League(league_id=args.league_id, year=args.year, username=args.username,
-                              password=args.password)
+    return ff_espn_api.League(
+        league_id=args.league_id,
+        year=args.year,
+        username=args.username,
+        password=args.password,
+    )
 
 
 def calc_optimal_score(matchup: ff_espn_api.Matchup, home: bool) -> float:
@@ -27,16 +43,16 @@ def calc_optimal_score(matchup: ff_espn_api.Matchup, home: bool) -> float:
 
     Note: this assumes standard lineup settings. We hope to fix this in the future.
     """
-    flex = 'RB/WR/TE'
+    flex = "RB/WR/TE"
     # TODO: this should probably contain Players instead of scores for further analysis
     optimal = {
-        'QB': [0],
-        'RB': [0, 0],
-        'WR': [0, 0],
-        'TE': [0],
+        "QB": [0],
+        "RB": [0, 0],
+        "WR": [0, 0],
+        "TE": [0],
         flex: [0],
-        'D/ST': [0],
-        'K': [0]
+        "D/ST": [0],
+        "K": [0],
     }
 
     lineup = matchup.home_lineup if home else matchup.away_lineup
@@ -56,7 +72,9 @@ def calc_optimal_score(matchup: ff_espn_api.Matchup, home: bool) -> float:
     return sum([sum(i) for i in itertools.zip_longest(*optimal.values(), fillvalue=0)])
 
 
-def process_season(league: ff_espn_api.League, verbose: bool = True) -> defaultdict(list):
+def process_season(
+    league: ff_espn_api.League, verbose: bool = True
+) -> defaultdict(list):
     """Calculate optimal scores and total team scores across a fantasy season."""
     res = defaultdict(list)
 
@@ -70,33 +88,38 @@ def process_season(league: ff_espn_api.League, verbose: bool = True) -> defaultd
             home_optimal = calc_optimal_score(matchup, True)
             home_scores = {}
             # actual score, from the players who started
-            home_scores['starters'] = matchup.home_score
+            home_scores["starters"] = matchup.home_score
             # score of the entire team, including bench
-            home_scores['whole_team'] = sum([player.points for player in matchup.home_lineup])
+            home_scores["whole_team"] = sum(
+                [player.points for player in matchup.home_lineup]
+            )
             # suboptimality = optimal lineup score - actual score
-            home_scores['suboptimality'] = home_optimal - home_scores['starters']
+            home_scores["suboptimality"] = home_optimal - home_scores["starters"]
             res[matchup.home_team].append(home_scores)
 
             away_optimal = calc_optimal_score(matchup, False)
             away_scores = {}
             # actual score, from the players who started
-            away_scores['starters'] = matchup.away_score
+            away_scores["starters"] = matchup.away_score
             # score of the entire team, including bench
-            away_scores['whole_team'] = sum([player.points for player in matchup.away_lineup])
+            away_scores["whole_team"] = sum(
+                [player.points for player in matchup.away_lineup]
+            )
             # actual score - optimal lineup score
             # suboptimality = optimal lineup score - actual score
-            away_scores['suboptimality'] = away_optimal - away_scores['starters']
+            away_scores["suboptimality"] = away_optimal - away_scores["starters"]
             res[matchup.away_team].append(away_scores)
 
     return res
 
 
-def get_sorted_suboptimals(results: defaultdict(list), verbose: bool = True
-                           ) -> list((ff_espn_api.Team, float)):
+def get_sorted_suboptimals(
+    results: defaultdict(list), verbose: bool = True
+) -> list((ff_espn_api.Team, float)):
     """Return a sorted list of how each team performed relative to the optimal lineup."""
     season_suboptimality = {}
     for team, scores in results.items():
-        season_suboptimality[team] = sum(week['suboptimality'] for week in scores)
+        season_suboptimality[team] = sum(week["suboptimality"] for week in scores)
     sorted_res = sorted(season_suboptimality.items(), key=lambda kv: (kv[1], kv[0]))
 
     if verbose:
@@ -107,13 +130,16 @@ def get_sorted_suboptimals(results: defaultdict(list), verbose: bool = True
     return sorted_res
 
 
-def get_sorted_team_scores(results: defaultdict(list), verbose: bool = True
-                           ) -> list((ff_espn_api.Team, float)):
+def get_sorted_team_scores(
+    results: defaultdict(list), verbose: bool = True
+) -> list((ff_espn_api.Team, float)):
     """Return a sorted list of how each team (including bench players) performed."""
     total_team_score = {}
     for team, scores in results.items():
-        total_team_score[team] = sum(week['whole_team'] for week in scores)
-    sorted_res = sorted(total_team_score.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+        total_team_score[team] = sum(week["whole_team"] for week in scores)
+    sorted_res = sorted(
+        total_team_score.items(), key=lambda kv: (kv[1], kv[0]), reverse=True
+    )
 
     if verbose:
         print("\nTotal team scores over the course of a season:")
@@ -123,15 +149,22 @@ def get_sorted_team_scores(results: defaultdict(list), verbose: bool = True
     return sorted_res
 
 
-def print_awards(league: ff_espn_api.League, sorted_suboptimals: list((ff_espn_api.Team, float)),
-                 sorted_team_scores: list((ff_espn_api.Team, float))) -> None:
+def print_awards(
+    league: ff_espn_api.League,
+    sorted_suboptimals: list((ff_espn_api.Team, float)),
+    sorted_team_scores: list((ff_espn_api.Team, float)),
+) -> None:
     """Print the team with the best lineups relative to optimal, and the most total team points."""
     print("\nAWARDS")
     print("------")
-    print(f"Coach of the year: {sorted_suboptimals[0][0].team_name}'s coach, whose starters" +
-          f" scored just {sorted_suboptimals[0][1]:.2f} less than optimal lineups.")
-    print(f"GM of the year: {sorted_team_scores[0][0].team_name}'s GM, whose team as a whole" +
-          f" scored {sorted_team_scores[0][1]:.2f} points in the {league.year} regular season.")
+    print(
+        f"Coach of the year: {sorted_suboptimals[0][0].team_name}'s coach, whose starters"
+        + f" scored just {sorted_suboptimals[0][1]:.2f} less than optimal lineups."
+    )
+    print(
+        f"GM of the year: {sorted_team_scores[0][0].team_name}'s GM, whose team as a whole"
+        + f" scored {sorted_team_scores[0][1]:.2f} points in the {league.year} regular season."
+    )
 
 
 def main():
