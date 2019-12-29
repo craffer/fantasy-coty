47e5a08012aed00d468bdf5f82a508b26610712b
Conor Rafferty <craffer@umich.edu>
2019-12-29 16:56:47 -0500

    Add SQL schema

---
+++ sql/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE seasons(
+    seasonid INTEGER PRIMARY KEY,
+    leagueid INTEGER NOT NULL,
+    year INTEGER NOT NULL,
+    processed BOOLEAN NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE teams(
+    teamid INTEGER PRIMARY KEY,
+    seasonid INTEGER NOT NULL,
+    teamname VARCHAR(31) NOT NULL,
+    owner VARCHAR(64) NOT NULL,
+    optimal REAL NOT NULL,
+    actual REAL NOT NULL,
+    coty_rank INTEGER NOT NULL,
+    gmoty_rank INTEGER NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(seasonid) REFERENCES seasons(seasonid)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
