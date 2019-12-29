d323fb72c22206d2ea34f91bd0e5e32ff3b70ebc
Conor Rafferty <craffer@umich.edu>
2019-12-29 16:56:57 -0500

    Add test data for SQL tables

---
+++ sql/testdata.sql
@@ -0,0 +1,7 @@
+-- add Delt W17 2019 season
+INSERT INTO seasons(leagueid, year, processed)
+VALUES (1371476, 2019, true);
+
+-- all team results
+INSERT INTO teams(seasonid, teamname, owner, optimal, actual, coty_rank, gmoty_rank)
+VALUES (1, "Scituate Smellers", "Conor Rafferty", 2000, 1000, 1, 3);
