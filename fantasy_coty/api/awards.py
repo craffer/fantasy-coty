b5ba5b2bc4dc2c52a033993e6e7018c7645bbc3c
Conor Rafferty <craffer@umich.edu>
2019-12-29 17:44:02 -0500

    Add POST route to start a job, with DB interaction

--- fantasy_coty/api/awards.py
+++ fantasy_coty/api/awards.py
@@ -5,10 +5,55 @@ from collections import defaultdict
 import ff_espn_api  # pylint: disable=import-error
 import flask  # pylint: disable=import-error
 import fantasy_coty
+from fantasy_coty.model import modify_db, query_db
 
 # map from league_id to (weeks processed, weeks total)
 running_jobs = {}
 jobs_mtx = threading.Lock()
+
+
+@fantasy_coty.app.route("/api/v1/start/", methods=["GET", "POST"])
+def start_processing():
+    """Start processing a season."""
+    context = {}
+
+    if flask.request.method == "POST":
+        # get the data from the POST request
+        league_info = flask.request.get_json()
+        league_id = league_info["league_id"]
+        year = league_info["year"]
+
+        query = "SELECT * FROM seasons WHERE leagueid = ? AND year = ?"
+        db_row = query_db(query, [league_id, year], one=True)
+
+        if not db_row:
+            # this season has never been processed, start it up
+            # TODO: check if they own the league first
+            jobs_mtx.acquire()
+            running_jobs[league_id] = None
+            jobs_mtx.release()
+
+            thread = threading.Thread(target=start_thread, args=[league_id, year])
+            thread.start()
+
+            # add this season to the database and mark it as in progress
+            query = "INSERT INTO seasons(leagueid, year, processed) VALUES (?, ?, ?);"
+            args = [league_id, year, False]
+            modify_db(query, args)
+            # re-query the database to get the newly inserted row
+            db_row = query_db(query, [league_id, year], one=True)
+
+        if not db_row["processed"]:
+            # it's currently being processed, return its current processing location
+            context["location"] = f"/api/v1/{league_id}/{year}/progress/"
+            return flask.jsonify(**context), 202
+        else:
+            # it's done already! serve them up the endpoint where the awards data is stored
+            context["location"] = f"/api/v1{league_id}/{year}/results/"
+            return flask.jsonify(**context), 200
+
+    # otherwise, redirect to search page
+    return flask.redirect(flask.url_for("TODO"))
 
 
 @fantasy_coty.app.route("/api/v1/<int:league_id>/<int:year>/start/", methods=["GET"])
